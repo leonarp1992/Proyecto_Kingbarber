2d902a5666a673662b135c0183fc2e1dc48a8aca
leonarp1992 <leonarp@uninorte.edu.co>
2021-12-10 00:01:39 -0500

    Actualizacion login

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,26 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import md5 from 'md5';
 import Cookies from 'universal-cookie/es6';
 
-const baseUrl="http://localhost:3001/usuarios";
+const baseUrl="http://localhost:3002/usuarios";
 const cookies = new Cookies();
-class Login extends Component {
+function Login() {
+    
+    const [user, setUser] = useState({
+        username: '',
+        pasword: ''
+    });
 
-    state={
-        form:{
-            username: '',
-            password: ''
-        }
-    }
-
-    handleChange= async e=>{
-        this.setState({
-            form:{
-                ...this.state.form,
-                [e.target.name]: e.target.value
-            }
+    const handleChange = e =>{
+        setUser(function(prev){
+            return ({...prev, [e.target.name]: e.target.value})
         });
-    }
+    };
 
-    iniciarSesion=async()=>{
-        await axios.get(baseUrl, {params:{usuario: this.state.form.username, contraseña: md5(this.state.form.password)}})
+    const iniciarSesion = () =>{
+        axios.get(baseUrl, {params:{usuario: user.username , contraseña: md5(user.password)}})
         .then(response=>{
             return response.data;
         })
@@ -52,85 +47,84 @@ class Login extends Component {
             return error;
         })
     }
-
-    render(){
-        return (
-            <div>
-              <main className="flex-shrink-0">
-                {/*<!-- Navigation-->*/}
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div className="container px-5">
-                        <img src={process.env.PUBLIC_URL + "/img/logo.png"} alt="Logo" width="90"/>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                                <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
-                                <li className="nav-item"><Link to="/quienessomos" className="nav-link">¿Quiénes Somos?</Link></li>                        
-                                <li className="nav-item"><Link to= "/servicios" className="nav-link">Servicios</Link></li>
-                                <li className="nav-item dropdown">
-                                          <li className="nav-link dropdown-toggle" id="navbarDropdownBlog" role="button" data-bs-toggle="dropdown" aria-expanded="false">Agenda Tu Cita</li>
-                                          <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
-                                              <li><Link to="/" className="dropdown-item" >Corte de cabello</Link></li>
-                                              <li><Link to="/" className="dropdown-item" >Diseño de barba</Link></li>
-                                              <li><Link to="/" className="dropdown-item" >Coloración de cabello o barba</Link></li>
-                                              <li><Link to="/" className="dropdown-item" >Manicure y pedicure</Link></li>
-                                              <li><Link to="/" className="dropdown-item" >Limpieza facial, nariz y oido</Link></li>
-                                          </ul>
-                                      </li>
-                                <li className="nav-item"><Link to="/login" className="nav-link" >Inicia Sesión</Link></li>
-                            </ul>
-                        </div>
+    
+    return (
+        <div>
+          <main className="flex-shrink-0">
+            {/*<!-- Navigation-->*/}
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container px-5">
+                    <img src={process.env.PUBLIC_URL + "/img/logo.png"} alt="Logo" width="90"/>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+                            <li className="nav-item"><Link to="/quienessomos" className="nav-link">¿Quiénes Somos?</Link></li>                        
+                            <li className="nav-item"><Link to= "/servicios" className="nav-link">Servicios</Link></li>
+                            <li className="nav-item dropdown">
+                                      <li className="nav-link dropdown-toggle" id="navbarDropdownBlog" role="button" data-bs-toggle="dropdown" aria-expanded="false">Agenda Tu Cita</li>
+                                      <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                          <li><Link to="/" className="dropdown-item" >Corte de cabello</Link></li>
+                                          <li><Link to="/" className="dropdown-item" >Diseño de barba</Link></li>
+                                          <li><Link to="/" className="dropdown-item" >Coloración de cabello o barba</Link></li>
+                                          <li><Link to="/" className="dropdown-item" >Manicure y pedicure</Link></li>
+                                          <li><Link to="/" className="dropdown-item" >Limpieza facial, nariz y oido</Link></li>
+                                      </ul>
+                                  </li>
+                            <li className="nav-item"><Link to="/login" className="nav-link" >Inicia Sesión</Link></li>
+                        </ul>
                     </div>
-                </nav>
-                {/*<!-- Features section-->*/}
-                  <section className="py-2" id="features">
-                      <div className="container px-5 my-10">
-                          <div className="py-2 text-center">
-                              <div className="container px-5 my-5">
-                                  <div className="row gx-5 justify-content-center">
-                                      <div className="col-lg-10 col-xl-7">
-                                          <div className="text-center">
-                                                   <div className="contenedor">
-                                                       <div className="input-contenedor">
-                                                          <i className="fas fa-envelope icon"></i>
-                                                          <input name="username" type="text" placeholder="Correo Electronico" onChange={this.handleChange}/>
-                                                       </div>
-                                                       <div className="input-contenedor">
-                                                          <i className="fas fa-key icon"></i>
-                                                          <input name="password" type="password" placeholder="Contraseña" onChange={this.handleChange}/>
-                                                       </div>
-                                                       <input type="button" value="Iniciar sesión" className="button" onClick={()=>this.iniciarSesion()}/>
-                                                       <p>¿No tienes una cuenta? <Link to="/registro" className="link">Registrate </Link></p>
+                </div>
+            </nav>
+            {/*<!-- Features section-->*/}
+              <section className="py-2" id="features">
+                  <div className="container px-5 my-10">
+                      <div className="py-2 text-center">
+                          <div className="container px-5 my-5">
+                              <div className="row gx-5 justify-content-center">
+                                  <div className="col-lg-10 col-xl-7">
+                                      <div className="text-center">
+                                               <div className="contenedor">
+                                                   <div className="input-contenedor">
+                                                      <i className="fas fa-envelope icon"></i>
+                                                      <input name="username" type="text" placeholder="Correo Electronico" onChange={handleChange}/>
                                                    </div>
-                                          </div>
+                                                   <div className="input-contenedor">
+                                                      <i className="fas fa-key icon"></i>
+                                                      <input name="password" type="password" placeholder="Contraseña" onChange={handleChange}/>
+                                                   </div>
+                                                   <input type="button" value="Iniciar sesión" className="button" onClick={iniciarSesion}/>
+                                                   <p>¿No tienes una cuenta? <Link to="/registro" className="link">Registrate </Link></p>
+                                               </div>
                                       </div>
                                   </div>
                               </div>
                           </div>
                       </div>
-                  </section>
-              </main>
-              {/*<!-- Footer-->*/}
-              <footer className="bg-dark py-4 mt-auto">
-                  <div className="container px-5">
-                      <div className="row align-items-center justify-content-between flex-column flex-sm-row">
-                          <div className="col-auto"><div className="small m-0 text-white">Copyright 2021 &copy; MisionTIC 2022</div></div>
-                          <div className="col-auto">
-                              <a className="link-light small" href="#!"><i className="bi bi-instagram"> @barberking</i></a>
-                              <span class="text-white mx-1">&middot;</span>
-                              <a className="link-light small" href="#!"><i className="bi bi-facebook"> BarbeKing</i></a>
-                              <span class="text-white mx-1">&middot;</span>
-                              <a className="link-light small" href="#!"><i className="bi bi-whatsapp"> 3009000800</i></a>
-                          </div>
+                  </div>
+              </section>
+          </main>
+          {/*<!-- Footer-->*/}
+          <footer className="bg-dark py-4 mt-auto">
+              <div className="container px-5">
+                  <div className="row align-items-center justify-content-between flex-column flex-sm-row">
+                      <div className="col-auto"><div className="small m-0 text-white">Copyright 2021 &copy; MisionTIC 2022</div></div>
+                      <div className="col-auto">
+                          <a className="link-light small" href="#!"><i className="bi bi-instagram"> @barberking</i></a>
+                          <span class="text-white mx-1">&middot;</span>
+                          <a className="link-light small" href="#!"><i className="bi bi-facebook"> BarbeKing</i></a>
+                          <span class="text-white mx-1">&middot;</span>
+                          <a className="link-light small" href="#!"><i className="bi bi-whatsapp"> 3009000800</i></a>
                       </div>
                   </div>
-              </footer>
-              {/*<!-- Bootstrap core JS-->*/}
-              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-              {/*<!-- Core theme JS-->*/}
-              <script src={process.env.PUBLIC_URL + "/js/scripts.js"}></script>
-            </div>
-            );
-    }
+              </div>
+          </footer>
+          {/*<!-- Bootstrap core JS-->*/}
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+          {/*<!-- Core theme JS-->*/}
+          <script src={process.env.PUBLIC_URL + "/js/scripts.js"}></script>
+        </div>
+        );
+    
 }
 export default Login;
